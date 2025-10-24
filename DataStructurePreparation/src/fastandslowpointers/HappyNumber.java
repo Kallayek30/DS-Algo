8380ab9813b968eab269fa55b4799560f2911e84
Kallayek30 <jishulayek@gmail.com>
2025-10-24 11:55:27 +0530

    Adding new line

--- DataStructurePreparation/src/fastandslowpointers/HappyNumber.java
+++ DataStructurePreparation/src/fastandslowpointers/HappyNumber.java
@@ -7,7 +7,6 @@ public class HappyNumber {
         for (int i = 0; i < a.length; i++) {
             System.out.println((i + 1) + ".\tInput Number: " + a[i]);
             String output = isHappyNumber(a[i]) ? "True" : "False";
-            System.out.println("Hi there");
             System.out.println("\n\tIs it a happy number? " + output);
             System.out.println(new String(new char[100]).replace('\0', '-'));
         }
